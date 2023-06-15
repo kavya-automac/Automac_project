574ad9099717a092e75f21127a70de4d4bffa502
neeraj <neeraj.automac@gmail.com>
2023-06-15 12:37:10 +0530

    machine_data_model

--- Automac_main/Automac_app/models.py
+++ Automac_main/Automac_app/models.py
@@ -9,3 +9,18 @@ from django.db import models
 #
 #     def __str__(self):   # to display the dept name in admin page
 #         return self.username
+
+
+class machine_data(models.Model):
+    objects = models.Manager()
+    machine_id = models.CharField(max_length=150)
+    machine_location= models.CharField(max_length=250)
+    timestamp= models.DateTimeField(auto_now_add=True)
+    digital_input= models.BooleanField(default=False)
+    digital_output= models.BooleanField(default=False)
+    analog_input= models.IntegerField()
+    analog_output= models.IntegerField()
+
+    def __str__(self):   # to display the timestamp in admin page
+             return  str(self.timestamp)
+
