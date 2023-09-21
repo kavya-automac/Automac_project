fa0774f3825e94f19df1bd9c0abb91f86d5f8f2e
kavya <setavakavya3@gmail.com>
2023-09-21 14:25:55 +0530

    kpi websocket

--- Automac_main/Automac_machines_app/kpi_websocket.py
+++ Automac_main/Automac_machines_app/kpi_websocket.py
@@ -7,99 +7,15 @@ from django.http import JsonResponse
 import json
 from datetime import datetime
 
-from Automac_app.models import all_Machine_data,Machines_List
+# from Automac_app.models import all_Machine_data,Machines_List
 from  . models import Machine_KPI_Data
 from asgiref.sync import sync_to_async
-#
-
-#
-# @sync_to_async
-# def kpi_socket(machine_id):
-#     print('kkk')
-#     kpis = []
-#
-#     try:
-#         user_data = all_Machine_data.objects.filter(machine_id__machine_id=machine_id)
-#         print('uers_data',user_data)
-#     except Machines_List.DoesNotExist:
-#         error_message = "Please enter a valid machine_id."
-#         return JsonResponse({"status": error_message}, status=400)  # Return an error response
-#     # if not user_data:  # Check if user_data is an empty list
-#     #     res=kpis.append("No data available for this machine.")
-#     #     print('res',res)
-#         # return JsonResponse(, status=200)
-#
-#     today = datetime.now().date()
-#
-#     for machine_data in user_data:
-#         # print('machine_data',machine_data)
-#         if machine_data.kpi is not None:
-#             kpitype = machine_data.kpi.Kpi_Type
-#             kpiname = machine_data.kpi.kpi_name
-#             kpi_labels = machine_data.kpi.labels
-#             kpi_result = {}
-#
-#             if kpitype == 'Line_Graph':
-#                 # Retrieve the latest record for line_graph
-#                 kpidata = Machine_KPI_Data.objects.filter(
-#                     machine_id=machine_data,
-#                     kpi_id__kpi_name=kpiname,
-#                     kpi_id__Kpi_Type=kpitype
-#                 ).order_by('-timestamp').first()
-#
-#                 kpi_result['x_axis'] = []
-#                 kpi_result['y_axis'] = []
-#
-#                 if kpidata:
-#                     time = kpidata.timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')  # Format timestamp as ISO string
-#                     value = kpidata.kpi_data
-#
-#                     kpi_result['x_axis'] = [time]
-#                     kpi_result['y_axis'] = [value]
-#             elif kpitype == 'cummulative':
-#                 # Retrieve the most recent record for today's date
-#                 kpidata = Machine_KPI_Data.objects.filter(
-#                     machine_id=machine_data,
-#                     kpi_id__kpi_name=kpiname,
-#                     kpi_id__Kpi_Type=kpitype,
-#                     timestamp__date=today
-#                 ).order_by('-timestamp').first()
-#
-#                 kpitype = 'text'
-#                 if kpidata:
-#                     name = kpidata.kpi_id.kpi_name
-#                     value = kpidata.kpi_data
-#
-#                     kpi_result['value'] = value
-#
-#                 # If 'value' is missing, set it to an empty string
-#                 if 'value' not in kpi_result:
-#                     kpi_result['value'] = ''
-#
-#             kpi_entry = {
-#                 'card': kpitype,
-#                 'title': kpiname,
-#                 'labels': kpi_labels,
-#                 'data': kpi_result,
-#             }
-#
-#             kpis.append(kpi_entry)
-#
-#     result_data = {
-#         'kpi': kpis,
-#     }
-#     print('result_data',result_data)
-#     res=json.dumps(result_data)
-#
-#     channel_layer = get_channel_layer()
-#     async_to_sync(channel_layer.group_send)(machine_id, {"type": "kpiweb", "text": res})
-#
-#         # print('channel layer', async_to_sync(channel_layer.group_send)(machine_id, {"type": "kpiweb", "text": res}))
-#
 
 @sync_to_async
 def kpi_socket(machine_id):
     print('kkk')
+    from Automac_app.models import all_Machine_data, Machines_List
+
     kpis = []
 
     try:
