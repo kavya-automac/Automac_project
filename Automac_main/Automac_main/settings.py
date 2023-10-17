7e6018ce04d25799379e68c13fa4cf26a2e6a9ec
kavya <setavakavya3@gmail.com>
2023-10-17 17:24:45 +0530

    setting.py(debug=False)

--- Automac_main/Automac_main/settings.py
+++ Automac_main/Automac_main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&svdzsu#n-4ir3!w3d=mle%2w31$=5%m(&6)0ortj)4kh1(&27'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ["65.0.154.172","127.0.0.1","192.168.29.144","192.168.94.216"]
