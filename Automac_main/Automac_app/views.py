ad04c23907b4bf00d02b66ee1f5d6750bbccfe07
kavya <setavakavya3@gmail.com>
2023-12-16 20:38:50 +0530

    added username in kpi rest_api

--- Automac_main/Automac_app/views.py
+++ Automac_main/Automac_app/views.py
@@ -567,6 +567,7 @@ class MachinesView(ViewSet):
                         data = kpis.get_kpis_data(user,machine)
                         data['machine_id']=machine.machine_id
                         data['machine_name']=machine.machine_name
+                        data['user_name'] =user
 
 
 
